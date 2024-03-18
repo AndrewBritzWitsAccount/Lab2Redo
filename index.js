4687c8c60cdb9e7ae4c6c277e577b9618d125e6f
Andrew Britz <2305109@students.wits.ac.za>
2024-03-18 07:29:44 +0200

    First Commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const path = require('path') // used later in the exercise
+const express = require('express')
+const app = express()
+
+// keep the first 3 lines from before
+// const mainRouter = express.Router()
+const mainRouter = require('./mainRoutes')
+
+app.use(mainRouter)
+
+const port = process.env.PORT || 3000
+app.listen(port)
+console.log('Express server running on port', port)
